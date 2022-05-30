d891dde4316ace6dbfcf3064db8fc9da7909f84f
longlg88 <longlg88@naver.com>
2022-05-30 09:54:03 +0900

    Update

--- test/provider.tf
+++ test/provider.tf
@@ -16,4 +16,8 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://s3.localhost.localstack.cloud:4566"
+  }
 }
--- test/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "role_arn" {
-  description = "role arn"
-  type        = string
-}
